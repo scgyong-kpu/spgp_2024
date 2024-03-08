16b05162de21e89cd71941f32c489c76499c47bc
none <none>
2024-03-08 11:56:38 +0900

    SampleJavaApp Skeleton

---
+++ a02/SampleJavaApp/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/samplejavaapp/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.tukorea.ge.spgp.scgyong.samplejavaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
