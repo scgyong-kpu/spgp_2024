123a0723af26dd08d7f3b3347640b96d6a9ff410
none <none>
2024-03-11 16:31:00 +0900

    Button click handling

--- a01/SampleJavaApp/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/samplejavaapp/MainActivity.java
+++ a01/SampleJavaApp/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/samplejavaapp/MainActivity.java
@@ -1,18 +1,27 @@
 package kr.ac.tukorea.ge.spgp.scgyong.samplejavaapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = findViewById(R.id.nameTextView);
-        tv.setText("Just Loaded");
+        Button btn = (Button)findViewById(R.id.pushMeButton);
+        btn.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        Log.d("Tag", "msg");
+        TextView tv = findViewById(R.id.nameTextView);
+        tv.setText("Button pushed");    }
 }
