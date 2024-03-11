13fff552c464114be8e575c1723fc2b76c76a5ec
none <none>
2024-03-11 17:09:36 +0900

    interface 를 설정하는 세번째 방법. 멤버 변수 활용하기

--- a01/SampleJavaApp/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/samplejavaapp/MainActivity.java
+++ a01/SampleJavaApp/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/samplejavaapp/MainActivity.java
@@ -1,7 +1,6 @@
 package kr.ac.tukorea.ge.spgp.scgyong.samplejavaapp;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,27 +9,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button btn = (Button)findViewById(R.id.pushMeButton);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TextView tv = findViewById(R.id.nameTextView);
-                tv.setText("Push Me");
-            }
-        });
+        btn.setOnClickListener(pushMeOnClickListener);
 
         Button btn2 = (Button)findViewById(R.id.anotherButton);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TextView tv = findViewById(R.id.nameTextView);
-                tv.setText("Another");
-            }
-        });
+        btn2.setOnClickListener(anotherOnClickListener);
     }
+    View.OnClickListener pushMeOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            TextView tv = findViewById(R.id.nameTextView);
+            tv.setText("Push Me");
+        }
+    };
+    private View.OnClickListener anotherOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            TextView tv = findViewById(R.id.nameTextView);
+            tv.setText("Another");
+        }
+    };
 }
