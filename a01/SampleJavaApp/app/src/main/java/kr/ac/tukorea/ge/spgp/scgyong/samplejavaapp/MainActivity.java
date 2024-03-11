913b77ac314c967ac85ef7e5856c976c6ca64be1
none <none>
2024-03-11 17:24:29 +0900

    Member Variable (Field) 활용하기.

--- a01/SampleJavaApp/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/samplejavaapp/MainActivity.java
+++ a01/SampleJavaApp/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/samplejavaapp/MainActivity.java
@@ -2,7 +2,6 @@ package kr.ac.tukorea.ge.spgp.scgyong.samplejavaapp;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,19 +9,20 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
 
+    private TextView nameTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameTextView = findViewById(R.id.nameTextView);
     }
 
     public void onBtnPushMe(View view) {
-        TextView tv = findViewById(R.id.nameTextView);
-        tv.setText("Push Me");
+        nameTextView.setText("Push Me");
     }
 
     public void onBtnAnthoer(View view) {
-        TextView tv = findViewById(R.id.nameTextView);
-        tv.setText("Another");
+        nameTextView.setText("Another");
     }
 }
