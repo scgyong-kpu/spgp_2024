8a0e721c698ec2a7df2d6e06f313f19a00be776a
none <none>
2024-03-12 16:18:36 +0900

    findViewById

--- a02/SampleJavaApp/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/samplejavaapp/MainActivity.java
+++ a02/SampleJavaApp/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/samplejavaapp/MainActivity.java
@@ -1,8 +1,9 @@
 package kr.ac.tukorea.ge.spgp.scgyong.samplejavaapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        String id = idInput.text;
+//        String id = idInput.getText();
+//        //tv.text = "Loaded";
+        TextView tv = findViewById(R.id.nameTextView);
+        tv.setText("Loaded");
     }
 }
