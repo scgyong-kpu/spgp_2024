86370a1e98e602ac9e36eeda77dca7d8a3144d59
none <none>
2024-03-12 17:18:32 +0900

    Member var 활용하여 findViewById 는 한번만.

--- a02/SampleJavaApp/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/samplejavaapp/MainActivity.java
+++ a02/SampleJavaApp/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/samplejavaapp/MainActivity.java
@@ -2,7 +2,6 @@ package kr.ac.tukorea.ge.spgp.scgyong.samplejavaapp;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,20 +9,21 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
 
+    private TextView nameTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameTextView = findViewById(R.id.nameTextView);
     }
 
     // #4. onClick XML attr 이용
     public void onBtnPushMe(View sender) {
-        TextView tv = findViewById(R.id.nameTextView);
-        tv.setText("PushMe");
+        nameTextView.setText("PushMe");
     }
 
     public void onBtnAnother(View view) {
-        TextView tv = findViewById(R.id.nameTextView);
-        tv.setText("Another");
+        nameTextView.setText("Another");
     }
 }
