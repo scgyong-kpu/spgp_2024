8d6ecce4c22fb20cd4eb85aad1c980387c86acd4
none <none>
2024-03-12 16:32:54 +0900

    Event 처리 방법 #1. 버튼 클릭 되면 나에게 알려줘

--- a02/SampleJavaApp/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/samplejavaapp/MainActivity.java
+++ a02/SampleJavaApp/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/samplejavaapp/MainActivity.java
@@ -1,11 +1,14 @@
 package kr.ac.tukorea.ge.spgp.scgyong.samplejavaapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,14 @@ public class MainActivity extends AppCompatActivity {
 //        //tv.text = "Loaded";
         TextView tv = findViewById(R.id.nameTextView);
         tv.setText("Loaded");
+
+        Button btn = findViewById(R.id.pushMeButton);
+        // #1
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.d("Tag", "button clicked");
     }
 }
