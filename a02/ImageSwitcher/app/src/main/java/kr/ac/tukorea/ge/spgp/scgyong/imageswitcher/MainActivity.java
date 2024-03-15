b6fcf8f864fad5aaa0ca2b494054a2479d444f99
none <none>
2024-03-15 11:35:09 +0900

    모범답안.

--- a02/ImageSwitcher/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/imageswitcher/MainActivity.java
+++ a02/ImageSwitcher/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/imageswitcher/MainActivity.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button prevButton;
+    private Button nextButton;
+    private TextView pageTextView;
+    private ImageView mainImageView;
     private int page;
     private int[] resIds = new int[] {
             R.mipmap.cat_1,
@@ -21,15 +27,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prevButton = findViewById(R.id.prevButton);
+        nextButton = findViewById(R.id.nextButton);
+        pageTextView = findViewById(R.id.pageTextView);
+        mainImageView = findViewById(R.id.mainImageView);
+
+        setPage(1);
+    }
+
+    private void setPage(int page) {
+        if (page < 1 || page > resIds.length) {
+            return;
+        }
+        this.page = page;
+        mainImageView.setImageResource(resIds[page-1]);
+        prevButton.setEnabled(page > 1);
+        nextButton.setEnabled(page < resIds.length);
+
+        pageTextView.setText(page + " / " + resIds.length);
     }
 
     public void onBtnPrev(View view) {
-        ImageView iv = findViewById(R.id.mainImageView);
-        iv.setImageResource(R.mipmap.cat_1);
+        setPage(page - 1);
     }
 
     public void onBtnNext(View view) {
-        ImageView iv = findViewById(R.id.mainImageView);
-        iv.setImageResource(R.mipmap.cat_2);
+        setPage(page + 1);
     }
 }
