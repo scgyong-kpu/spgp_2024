31bf4559a58096ff7f7b206248d4387a064a20eb
none <none>
2024-03-15 13:26:28 +0900

    모범답안.

--- a01/ImageSwitcher2/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/imageswitcher/MainActivity.java
+++ a01/ImageSwitcher2/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/imageswitcher/MainActivity.java
@@ -4,24 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView mainImageView;
+    private TextView pageTextView;
+    private Button prevButton;
+    private Button nextButton;
+
+    int page;
+
+    private static int[] resIds = new int[] {
+            R.mipmap.cat_1, R.mipmap.cat_2, R.mipmap.cat_3,
+            R.mipmap.cat_4, R.mipmap.cat_5, R.mipmap.cat_6,
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //R.mipmap.cat_1
+        mainImageView = findViewById(R.id.mainImageView);
+        pageTextView = findViewById(R.id.pageTextView);
+        prevButton = findViewById(R.id.prevButton);
+        nextButton = findViewById(R.id.nextButton);
+
+        setPage(1);
+    }
+
+    private void setPage(int page) {
+        if (page < 1 || page > resIds.length) {
+            return;
+        }
+        this.page = page;
+        mainImageView.setImageResource(resIds[page - 1]);
+        pageTextView.setText(page + " / " + resIds.length);
+
+        prevButton.setEnabled(page > 1);
+        nextButton.setEnabled(page < resIds.length);
     }
 
     public void onBtnPrevious(View view) {
-        ImageView iv = findViewById(R.id.mainImageView);
-        iv.setImageResource(R.mipmap.cat_1);
+        setPage(page - 1);
     }
 
     public void onBtnNext(View view) {
-        ImageView iv = findViewById(R.id.mainImageView);
-        iv.setImageResource(R.mipmap.cat_2);
+        setPage(page + 1);
     }
 }
