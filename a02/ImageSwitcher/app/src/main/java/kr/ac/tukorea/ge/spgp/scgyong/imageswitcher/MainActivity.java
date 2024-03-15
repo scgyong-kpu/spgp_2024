bbe9bf5f548a30f531ee5a8aa432438c3815337c
none <none>
2024-03-15 11:18:54 +0900

    첫번째 버튼에는 고양이1, 두번째 버튼에 고양이 2가 나오게 한다.

--- a02/ImageSwitcher/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/imageswitcher/MainActivity.java
+++ a02/ImageSwitcher/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/imageswitcher/MainActivity.java
@@ -3,6 +3,8 @@ package kr.ac.tukorea.ge.spgp.scgyong.imageswitcher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnPrev(View view) {
+        ImageView iv = findViewById(R.id.mainImageView);
+        iv.setImageResource(R.mipmap.cat_1);
+    }
+
+    public void onBtnNext(View view) {
+        ImageView iv = findViewById(R.id.mainImageView);
+        iv.setImageResource(R.mipmap.cat_2);
     }
 }
