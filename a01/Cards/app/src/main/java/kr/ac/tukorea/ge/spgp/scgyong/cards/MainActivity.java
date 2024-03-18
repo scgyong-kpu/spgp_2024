117154b2de67949a8e66e4bce850aa64fe23f6e6
none <none>
2024-03-18 16:09:33 +0900

    눌린 버튼은 A Spade 로 바꾼다.

--- a01/Cards/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cards/MainActivity.java
+++ a01/Cards/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cards/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void onBtnCard(View view) {
         Log.d("Tag", "Card clicked. ID=" + view.getId());
+
+        ImageView imageView = (ImageView)view;
+        imageView.setImageResource(R.mipmap.card_as);
     }
 }
