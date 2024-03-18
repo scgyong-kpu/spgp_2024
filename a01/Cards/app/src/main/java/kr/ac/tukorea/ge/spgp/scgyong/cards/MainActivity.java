e8c370e1cf96b708109937d970530f30fe8cdffa
none <none>
2024-03-18 16:38:07 +0900

    ImageButton 이 보여주어야 하는 mipmap resource id 를 tag 로 매달아 두고 눌렸을 때 그 이미지를 보여준다.

--- a01/Cards/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cards/MainActivity.java
+++ a01/Cards/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cards/MainActivity.java
@@ -5,33 +5,60 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView previousCardImageView;
+    private ImageButton previousCardImageView;
 
+    private static int[] RES_IDS = new int[] {
+            R.mipmap.card_2c, R.mipmap.card_3d, R.mipmap.card_4h, R.mipmap.card_5s,
+            R.mipmap.card_jc, R.mipmap.card_qh, R.mipmap.card_kd, R.mipmap.card_as,
+            R.mipmap.card_2c, R.mipmap.card_3d, R.mipmap.card_4h, R.mipmap.card_5s,
+            R.mipmap.card_jc, R.mipmap.card_qh, R.mipmap.card_kd, R.mipmap.card_as,
+    };
+    private static int[] VIEW_IDS = new int[] {
+            R.id.card_00, R.id.card_01, R.id.card_02, R.id.card_03,
+            R.id.card_10, R.id.card_11, R.id.card_12, R.id.card_13,
+            R.id.card_20, R.id.card_21, R.id.card_22, R.id.card_23,
+            R.id.card_30, R.id.card_31, R.id.card_32, R.id.card_33,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        for (int i = 0; i < VIEW_IDS.length; i++) {
+            ImageView iv = findViewById(VIEW_IDS[i]);
+            // #1
+//            int value = RES_IDS[i];
+//            Integer intObj = new Integer(value);
+            // #2
+//            Integer intObj = RES_IDS[i];
+//            iv.setTag(intObj);
+            // #3
+            iv.setTag(RES_IDS[i]);
+        }
     }
 
     public void onBtnCard(View view) {
         Log.d("Tag", "Card clicked. ID=" + view.getId());
 
-        ImageView imageView = (ImageView)view;
-        if (imageView == previousCardImageView) {
+        ImageButton imageButton = (ImageButton)view;
+        if (imageButton == previousCardImageView) {
             return;
         }
+
+        int imgRes = (Integer)imageButton.getTag();
         if (previousCardImageView != null) {
             previousCardImageView.setImageResource(R.mipmap.card_blue_back);
         }
 
-        imageView.setImageResource(R.mipmap.card_as);
+        imageButton.setImageResource(imgRes);
 
-        previousCardImageView = imageView;
+        previousCardImageView = imageButton;
 
     }
 }
