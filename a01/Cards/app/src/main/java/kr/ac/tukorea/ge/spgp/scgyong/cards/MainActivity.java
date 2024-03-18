ca074ea9ba8efb95a1655a1c4f3664fd8270188d
none <none>
2024-03-18 16:20:03 +0900

    한 장만 Ace 가 나오게 하고 이전 것은 뒷면으로 되돌린다.

--- a01/Cards/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cards/MainActivity.java
+++ a01/Cards/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cards/MainActivity.java
@@ -9,6 +9,9 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView previousCardImageView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
         Log.d("Tag", "Card clicked. ID=" + view.getId());
 
         ImageView imageView = (ImageView)view;
+        if (imageView == previousCardImageView) {
+            return;
+        }
+        if (previousCardImageView != null) {
+            previousCardImageView.setImageResource(R.mipmap.card_blue_back);
+        }
+
         imageView.setImageResource(R.mipmap.card_as);
+
+        previousCardImageView = imageView;
+
     }
 }
