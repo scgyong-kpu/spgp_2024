2b5380d49b282444176578717c944157684be925
Kim, Kiyong <scgyong@tukorea.ac.kr>
2024-03-19 16:13:45 +0900

    한 장만 Ace 를 보여준다. 이전것은 뒷면으로.

--- a02/Cards/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cards/MainActivity.java
+++ a02/Cards/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cards/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton previousImageButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +20,15 @@ public class MainActivity extends AppCompatActivity {
     public void onBtnCard(View view) {
         Log.d("Tag", "Card!!" + view.getId());
 
+        if (previousImageButton != null) {
+            previousImageButton.setImageResource(R.mipmap.card_blue_back);
+        }
+
         //ImageButton imageButton = (ImageButton) view;
         ImageButton imageButton = (ImageButton) view;
         imageButton.setImageResource(R.mipmap.card_as);
+
+        previousImageButton = imageButton;
+
     }
 }
